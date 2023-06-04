c191fb44c3043975945399bd5ab1b2e5d75c9ef4
Benjamin Khoury <khourybenjamin@gmail.com>
2023-06-04 14:25:20 +0100

    add jwt strategy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_131056) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_132235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_131056) do
     t.integer "activity_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
